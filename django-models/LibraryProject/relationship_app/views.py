e47ffd656b54d6165b3a247b41b712daee8a8054
samuel mwangi <samuelmwangi729@gmail.com>
2025-09-07 10:47:33 +0300

    finished the second one

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render,get_object_or_404
-from django.http import request
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
 
