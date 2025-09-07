6edecf92652309c80e1d46315eef8b1b086c9832
samuel mwangi <samuelmwangi729@gmail.com>
2025-09-07 12:46:29 +0300

    completed the authentication

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -19,6 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("book",include("bookshelf.urls")),
-    path("",include("relationship_app.urls"))
+    path("book/",include("bookshelf.urls")),
+    path("",include("relationship_app.urls")),
+    path("",include("user.urls")),
 ]
--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,8 +2,7 @@
 
 from django.urls import path
 from . import views
-from .views import list_books
 urlpatterns = [
-    path('books/', list_books, name='list_books'),
+    path('books/', views.list_books, name='list_books'),
     path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
