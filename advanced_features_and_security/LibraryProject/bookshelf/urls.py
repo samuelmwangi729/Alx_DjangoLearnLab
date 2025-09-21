f65058644efbc33434c9e6ea4a9e54636757573d
samuel mwangi <samuelmwangi729@gmail.com>
2025-09-21 18:59:36 +0300

    defined the permissions

--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("all", views.book_list, name="index"),
+    path("create", views.create, name="index"),
+    path("edit/<int:pk>/", views.edit, name="index"),
+    path("delete/<int:pk>", views.delete, name="index"),
 ]
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from relationship_app.models import Book
 # Create your views here.
-@permission_required('app_name.can_edit', raise_exception=True)
-def index(request):
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def book_list(request):
     books = Book.objects.select_related("author").all()
     return render(request,"book.html",{"books":books})
+def create(request):
+    pass
+def delete(request):
+    pass
+def edit(request):
+    pass
