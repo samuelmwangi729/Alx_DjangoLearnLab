12a912faf388816577874f65967786fefb1493fc
samuel mwangi <samuelmwangi729@gmail.com>
2025-09-21 19:20:07 +0300

    created the form

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -3,13 +3,7 @@
 from django import forms
 from .models import Book
 
-class BookForm(forms.ModelForm):
+class ExampleForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'publication_year']
-
-        widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Book Title'}),
-            'author': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Author Name'}),
-            'publication_year': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Year Published'}),
-        }
