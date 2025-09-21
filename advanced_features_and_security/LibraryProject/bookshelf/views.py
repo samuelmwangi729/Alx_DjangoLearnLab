7f620bacc59f403756bf76cc2071c06e0a07ace5
samuel mwangi <samuelmwangi729@gmail.com>
2025-09-21 19:24:29 +0300

    created the form

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from relationship_app.models import Book
-from bookshelf.forms import ExampleForm
+from .forms import ExampleForm
 
 # Create your views here.
 @permission_required('bookshelf.can_edit', raise_exception=True)
@@ -17,7 +17,7 @@ def create(request):
     else:
         form = ExampleForm()  # Empty form for GET request
 
-    return render(request, 'bookshelf/create.html', {'form': form})
+    return render(request, 'bookshelf/form_example.html', {'form': form})
 def delete(request):
     pass
 def edit(request):
