ffeb07be25b9357d611ee3a2b8c294a79a3d9af4
samuel mwangi <samuelmwangi729@gmail.com>
2025-09-21 19:23:11 +0300

    created the form

--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("all", views.book_list, name="index"),
-    path("create", views.create, name="index"),
-    path("edit/<int:pk>/", views.edit, name="index"),
-    path("delete/<int:pk>", views.delete, name="index"),
+    path("all", views.book_list, name="book_list"),
+    path("create", views.create, name="books.create"),
+    path("edit/<int:pk>/", views.edit, name="book.edit"),
+    path("delete/<int:pk>", views.delete, name="book.delete"),
 ]
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from relationship_app.models import Book
+from bookshelf.forms import ExampleForm
+
 # Create your views here.
 @permission_required('bookshelf.can_edit', raise_exception=True)
 def book_list(request):
     books = Book.objects.select_related("author").all()
     return render(request,"book.html",{"books":books})
 def create(request):
-    pass
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            form.save()  # Save the new Book instance
+            return redirect('book_list')  # Redirect to a book list or any page you want
+    else:
+        form = ExampleForm()  # Empty form for GET request
+
+    return render(request, 'bookshelf/create.html', {'form': form})
 def delete(request):
     pass
 def edit(request):
