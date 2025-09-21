3839951499a76a5eee1964aa2797845748401adf
samuel mwangi <samuelmwangi729@gmail.com>
2025-09-21 18:45:25 +0300

    defined the permissions

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
 from relationship_app.models import Book
 # Create your views here.
+@permission_required('app_name.can_edit', raise_exception=True)
 def index(request):
     books = Book.objects.select_related("author").all()
     return render(request,"book.html",{"books":books})
