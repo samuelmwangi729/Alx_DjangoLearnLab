fb4c1205a79043c61778e8b35bf8c784ce2f50f5
samuel mwangi <samuelmwangi729@gmail.com>
2025-09-22 05:20:15 +0300

    Used the books viewset for CRUD operations

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets
 from .serializers import BookSerializer
 from .models import Book
 
@@ -7,6 +7,6 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-class BookViewSet(ModelViewSet):
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
