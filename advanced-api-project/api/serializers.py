2efd5b6f79b3af04758ece65554774f2e5c1130c
samuel mwangi <samuelmwangi729@gmail.com>
2025-09-23 07:29:01 +0300

    feat:added (many=True, read_only=True) to the authors serializer

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -17,7 +17,7 @@ class BookSerializer(serializers.ModelSerializer):
 #Author serializer
 
 class AuthorSerializer(serializers.ModelSerializer):
-    books  = BookSerializer(many=True)
+    books  = BookSerializer(many=True, read_only=True)
     class Meta:
         model=Author
         fields=['name','books']
