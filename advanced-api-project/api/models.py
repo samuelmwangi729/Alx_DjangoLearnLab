e80b6a4c59701659893ba384658bef2461fb2aaf
samuel mwangi <samuelmwangi729@gmail.com>
2025-09-23 07:26:44 +0300

    feat: renamed the project

---
+++ advanced-api-project/api/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+#book model here 
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    publication_year= models.DateField()
+    author = models.ForeignKey(Author,on_delete=models.CASCADE,related_name='books')
+
+    def __str__(self):
+        return self.title
---
+++ advanced-api-project/api/serializers.py
@@ -0,0 +1,24 @@
+from django.forms import fields
+from rest_framework import serializers
+from .models import Author,Book
+from datetime import datetime
+
+#Books Serializer
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Book
+        fields='__all__'
+    def validate(self, value):
+        current_year = datetime.now().year
+        if value > current_year:
+            raise serializers.ValidationError("Publication year cannot be in the future.")
+        return value
+        
+#Author serializer
+
+class AuthorSerializer(serializers.ModelSerializer):
+    books  = BookSerializer(many=True)
+    class Meta:
+        model=Author
+        fields=['name','books']
+
