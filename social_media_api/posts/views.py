daecff66ec876a3205a6c189d16779a047b6509d
samuel mwangi <samuelmwangi729@gmail.com>
2025-10-10 18:20:38 +0300

    completed

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -46,7 +46,7 @@ class CommentViewSet(viewsets.ModelViewSet):
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def user_feed(request):
-    followed_users = request.user.following.all()
-    posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')
+    following_users = request.user.following.all()
+    posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
     serializer = PostSerializer(posts, many=True)
     return Response(serializer.data)
