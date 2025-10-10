f2be2ab4380cacec74a15b8f94efdcf991217caa
samuel mwangi <samuelmwangi729@gmail.com>
2025-10-10 17:35:50 +0300

    feat: completed  the second task

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth import get_user_model
 class Post(models.Model):
     title = models.CharField(blank=False)
     content = models.TextField()
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(get_user_model(),on_delete=models.CASCADE,blank=False,null=False)
 
     def __str__(self):
@@ -16,8 +16,8 @@ class Comment(models.Model):
     post = models.ForeignKey(Post,on_delete=models.CASCADE)
     user = models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
     content = models.TextField()
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.content
