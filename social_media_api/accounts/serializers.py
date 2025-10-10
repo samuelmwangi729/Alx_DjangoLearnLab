209dd4b7cf5d7bb902a2de031cb99266416fd641
samuel mwangi <samuelmwangi729@gmail.com>
2025-10-10 16:25:01 +0300

    feat:initialized the project

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,20 +1,25 @@
 # accounts/serializers.py
 
 from rest_framework import serializers
-from .models import CustomUser
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, get_user_model
 from rest_framework.authtoken.models import Token
+
+User = get_user_model()  # ✅ Proper way to reference the user model
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = CustomUser
+        model = User
         fields = ('username', 'email', 'password', 'bio', 'profile_picture')
 
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(
-            **validated_data
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture')
         )
         Token.objects.create(user=user)
         return user
